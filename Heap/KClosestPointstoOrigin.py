8c070592d31c7bd00db0a0c2a2e4d8d354c4e29b
MinhNguyenOH <imduyminh@gmail.com>
2024-06-22 15:55:08 -0400

    K Closest Points to Origin

--- Heap/KClosestPointstoOrigin.py
+++ Heap/KClosestPointstoOrigin.py
@@ -2,7 +2,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         min_heap = []
         for x, y in points:
-            dist = (x**2) + (y**2)
+            dist = x**2 + y**2
             min_heap.append((dist, x, y))
         heapq.heapify(min_heap)
         res = []
